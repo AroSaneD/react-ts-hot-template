51a3feec4b5d30ec3d6d3521f005de8472be6ba6
AroSaneD <mail.junk.3135@gmail.com>
2018-01-21 01:38:02 +0200

    added code example for working with canvas and hmr

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,41 @@ import * as React from "react";
 
 export default class App extends React.Component<{}> {
     interval: number;
-    state: { count: number } = null;
+    state: any = null;
+    context: CanvasRenderingContext2D = null;
 
     constructor(props: any) {
         super(props);
-        this.state = { count: 0 };
+        this.state = {
+            count: 0,
+            posx: 50,
+            posy: 50
+        };
     }
 
     //This state will be maintained during hot reloads
     componentWillMount() {
-        this.interval = window.setInterval(() => { this.tick() }, 1000);
+        this.interval = window.setInterval(() => { this.tick() }, 16);
+    }
+
+    componentDidMount() {
+        var canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+        var context = canvas.getContext("2d");
+        this.context = context;
     }
 
     tick() {
-        this.setState({ count: this.state.count + 5 })
+        this.setState({
+            count: (this.state.count + 1) % 300,
+            posx: (this.state.posx + 1) % 300,
+            posy: (this.state.posy + 0.25) % 200
+        })
+
+        if (this.context != null) {
+            this.context.clearRect(0, 0, 500, 500);
+            this.context.fillStyle = "green";
+            this.context.fillRect(this.state.posx, this.state.posy, 40, 40);
+        }
     }
 
     componentWillUnmount() {
@@ -26,6 +47,7 @@ export default class App extends React.Component<{}> {
     render() {
         return (
             <div>
+                <canvas id="gameCanvas" width="500" height="500"></canvas>
                 <h2>Preserved state counter: {this.state.count} + 123 = {this.state.count + 123}</h2>
             </div>
         );
